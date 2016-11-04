6b40b7dd9fffa0e72b3708db7a9016043a1c9f24
Paintface07 <paintface07@gmail.com>
2016-11-04 18:17:00 -0500

    start adding config loader

--- src/main/java/org/kondrak/archer/bot/ArcherMain.java
+++ src/main/java/org/kondrak/archer/bot/ArcherMain.java
@@ -1,11 +1,15 @@
 package org.kondrak.archer.bot;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
 import org.kondrak.archer.bot.listener.MessageListener;
 import org.postgresql.ds.PGPoolingDataSource;
 import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.api.events.EventDispatcher;
 import sx.blah.discord.util.DiscordException;
+
+import java.io.IOException;
 
 /**
  * Created by Administrator on 11/3/2016.
@@ -21,6 +25,12 @@ public class ArcherMain {
             datasource.setUser(args[1]);
             datasource.setPassword(args[2]);
             datasource.setMaxConnections(5);
+
+            try {
+                String config = Resources.toString(Resources.getResource("config.json"), Charsets.UTF_8);
+            } catch(IOException ex) {
+                throw new RuntimeException("Could not read configuration");
+            }
 
             IDiscordClient client = getClient(args[0], true);
             EventDispatcher dispatcher = client.getDispatcher();
